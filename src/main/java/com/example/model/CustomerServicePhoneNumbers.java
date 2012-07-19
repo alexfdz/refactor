b686e01441dd6152b7ab4aa421a4158b5d626f71
Alex <alexfdz@gmail.com>
2012-07-19 20:24:14 +0100

    Customer service phone numbers wrapper (spring mvc? validations? more clear)

---
+++ src/main/java/com/example/model/CustomerServicePhoneNumbers.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import java.util.Set;
+
+public class CustomerServicePhoneNumbers {
+
+	private Set<String> phoneNumbers;
+
+	public Set<String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Set<String> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+}
